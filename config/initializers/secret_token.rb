9d0e6b8b8357f0c7be3c3af9db7c2eefc648ef6d
Andrei <kezek.ma@gmail.com>
2013-02-05 11:07:08 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoviesRails::Application.config.secret_token = 'b02c73349836e91855033fd7e33aa65b924fb5b374f04d4630459de860c6a69ee136d389dc78393112ff10a180c60e8562212e5cf14fdbf13d2235a5028fff65'
