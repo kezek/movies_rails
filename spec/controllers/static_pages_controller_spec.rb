35b16c7dd487afcb62a61ace57eec9e4a1187386
Andrei M <kezek.ma@gmail.com>
2013-03-03 17:48:23 +0200

    closes #5 + fixed views location

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,29 +1,5 @@
 require 'spec_helper'
 
 describe StaticPagesController do
-  describe "GET 'home' action from 'StaticPagesController'" do
-    it "should be successful" do
-      get 'home'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'contact' action from 'StaticPagesController'" do
-    it "should be successful" do
-      get 'contact'
-      response.should be_success
-    end
-  end
-
-  describe "GET homepage" do
-    it "should route to static_pages#home" do
-      { get: "/" }.should route_to("static_pages#home")
-    end
-  end
-
-  describe "GET '/contact'" do
-    it "should route to static_pages#contact" do
-      { get: "/contact"}.should route_to("static_pages#contact")
-    end
-  end
+  pending 'TODO'
 end
