fd251b34086b693c0fad71d60a4916e69607719d
Andrei M <kezek.ma@gmail.com>
2013-08-13 23:18:39 +0300

    adding testing methods and factory class for users_controller

--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -1,10 +1,17 @@
 require 'spec_helper'
+# http://everydayrails.com/2012/04/07/testing-series-rspec-controllers.html
 
 describe Api::UsersController do
   describe "GET #index" do
-    it "should return an a JSON" do
+    it "should return an a JSON and be successful" do
       get :index
       response.headers['Content-Type'].should include 'application/json'
+      response.should be_success
+    end
+    it 'should return one entry' do
+      FactoryGirl.create(:user)
+      get :index
+      puts ActiveSupport::JSON.decode(response.body).length.should == 1
     end
 
   end
---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :user do
+    email {"test11@test.ro"}
+    password {'test11'}
+  end
+end
