1c48b8f5064f7ef0d1c480a954f3b72e62099c1f
Andrei M <kezek.ma@gmail.com>
2013-08-19 20:11:04 +0300

    adding validations to user model and UT

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password
-  #validates_uniqueness_of :email
+  validates_uniqueness_of :email
+  validates :email, :presence => true
+  validates :password, :presence => true
 end
