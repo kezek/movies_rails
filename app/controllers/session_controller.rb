4263ebd78f21d998d341ba61126c703c321edcb1
Andrei M <kezek.ma@gmail.com>
2013-10-07 21:59:27 +0300

    adding proper redirect_url after succesfull login

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -9,7 +9,7 @@ class SessionController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:email] = user.email
-      redirect_to 'login', notice: "Logged in!"
+      redirect_to root_url, :notice => "Logged in!"
     else
       flash.now.alert = "Email or password is invalid"
       render "session/login"
